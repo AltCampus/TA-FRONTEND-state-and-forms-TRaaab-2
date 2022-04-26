cd82b0e41120a39b583233d83efa19de258f575b
PujaGKundu <pujakundu100@gmail.com>
2022-04-26 13:58:57 +0530

    Re-factor Shopping Cart

--- block-BRaabw/shopping-cart/src/components/Aside.js
+++ block-BRaabw/shopping-cart/src/components/Aside.js
@@ -1,91 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data.json";
 import Products from "./Products";
 
-class Aside extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      size: "",
-      order: "select",
-    };
-  }
+function Aside() {
+  let [size, setSize] = useState(null);
+  let [order, setOrder] = useState("select");
 
-  handleChange = (event) => {
-    this.setState({ order: event.target.value });
+  const handleChange = (event) => {
+    setOrder(event.target.value);
   };
 
-  handleClick = (size) => {
-    this.setState({
-      size: size,
-    });
+  const handleClick = (size) => {
+    setSize(size);
   };
 
-  render() {
-    let products = data.products;
-    let sizes = products.reduce((acc, size) => {
-      acc = acc.concat(size.availableSizes);
-      return acc;
-    }, []);
-    let unique = [...new Set(sizes)];
-    let item;
-    if (this.state.size) {
-      item = [...products].filter((product) =>
-        product.availableSizes.find((size) => size === this.state.size)
-      );
-    } else if (this.state.order === "select") {
-      item = products;
-    } else if (this.state.order === "lowest") {
-      item = [...products]
-        .filter((product) => product.price)
-        .sort(function (a, b) {
-          return a - b;
-        });
-      console.log(item);
-    } else if (this.state.order === "highest") {
-      item = [...products]
-        .filter((product) => product.price)
-        .sort(function (a, b) {
-          return b - a;
-        });
-    } else {
-      item = products;
-    }
-    return (
-      <>
-        <div className="flex evenly">
-          <h3>{item.length} Products found</h3>
-          <form>
-            <label htmlFor="order">Order By: </label>
-            <select onChange={this.handleChange}>
-              <option value="select">Select</option>
-              <option value="lowest">Lowest to Highest</option>
-              <option value="highest">Highest to Lowest</option>
-            </select>
-          </form>
-        </div>
-        <div className="flex">
-          <aside className="flex-30">
-            <div className="grid">
-              {unique.map((size, i) => (
-                <p
-                  key={i}
-                  className={
-                    this.state.size === size ? "circle active" : "circle"
-                  }
-                  onClick={() => this.handleClick(size)}
-                >
-                  {size}
-                </p>
-              ))}
-            </div>
-          </aside>
+  let products = data.products;
+  let sizes = products.reduce((acc, size) => {
+    acc = acc.concat(size.availableSizes);
+    return acc;
+  }, []);
+  let unique = [...new Set(sizes)];
 
-          <Products item={item} />
-        </div>
-      </>
+  let item;
+  if (size) {
+    item = [...products].filter((product) =>
+      product.availableSizes.find((s) => s === size)
     );
+  } else if (order === "select") {
+    item = products;
+  } else if (order === "lowest") {
+    item = [...products].sort(function (a, b) {
+      return a.price - b.price;
+    });
+  } else if (order === "highest") {
+    item = [...products].sort(function (a, b) {
+      return b.price - a.price;
+    });
+  } else {
+    item = products;
   }
+
+  return (
+    <>
+      <div className="flex evenly">
+        <h3>{item.length} Products found</h3>
+        <form>
+          <label htmlFor="order">Order By: </label>
+          <select onChange={handleChange}>
+            <option value="select">Select</option>
+            <option value="lowest">Lowest to Highest</option>
+            <option value="highest">Highest to Lowest</option>
+          </select>
+        </form>
+      </div>
+      <div className="flex">
+        <aside className="flex-30">
+          <div className="grid">
+            {unique.map((s, i) => (
+              <p
+                key={i}
+                className={s === size ? "circle active" : "circle"}
+                onClick={() => handleClick(s)}
+              >
+                {s}
+              </p>
+            ))}
+          </div>
+        </aside>
+
+        <Products item={item} size={size} />
+      </div>
+    </>
+  );
 }
 
 export default Aside;
